861f05c9c1d938872933ae9bd68c288bd7ace28c
Navkaran Singh <39305757+navkarans05@users.noreply.github.com>
2021-02-05 12:32:14 +0530

    Update prac5.java

--- prac5.java
+++ prac5.java
@@ -2,10 +2,9 @@ package practice_pg;
 import java.util.*;
 public class prac5 {
 	public static void main(String [] args) {
-		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		
-		System.out.println(str);
+		int a=123,b=123;
+		if(a==b){
+			System.out.println(true);
+		}
 	}
-
 }
